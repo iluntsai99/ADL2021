747b5440581073ec9e85ed2f47c00991f5f2241a
iluntsai99 <b06902135@ntu.edu.tw>
2021-04-10 15:21:28 +0800

    preparing scripts for reproduce

---
+++ HW1/intent_cls.sh
@@ -0,0 +1,5 @@
+# bash ./intent_cls.sh /path/to/test.json /path/to/pred.csv
+# "${1}": path to the testing file.
+# "${2}": path to the output predictions.
+
+python3.8 test_intent.py --test_file "${1}" --ckpt_path ckpt/intent/91377.pt --pred_file "${2}"
